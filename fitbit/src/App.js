d039800afaea08dd0cbebef27d7b9dd5d992ff70
Prem Kumar <prembammidi25@msitprogram.net>
2020-07-02 04:42:34 +0530

    git inital setup

---
+++ fitbit/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import FileDownlaod from 'js-file-download';
+
+class FitBit extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: "",
+      startTime: "",
+      endTime: "",
+    }
+  }
+
+}
+
+export default FitBit
+
+
+
