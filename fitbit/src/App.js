241a74407b119c7c9b06885d9061766678a5d7a0
Prem Kumar <prembammidi25@msitprogram.net>
2020-07-02 10:01:51 +0530

    details commit

--- fitbit/src/App.js
+++ fitbit/src/App.js
@@ -16,6 +16,77 @@ class FitBit extends React.Component {
     }
   }
 
+  handleDate = (event) => {
+    this.setState({
+      date: event.target.value
+    });
+  }
+
+  handleStartTime = (event) => {
+    this.setState({
+      startTime: event.target.value
+    });
+  }
+
+  handleEndTime = (event) => {
+    this.setState({
+      endTime: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1 style={{textAlign: "center"}}>My Fit Bit Details</h1>
+        </div>
+        <div style={{backgroundColor: "lightblue", padding : 100, margin : 100, textAlign: "center"}}>
+          <form>
+            <div>
+              <h4>Enter the date : </h4>
+              <InputGroup className='mb-3'>
+                <FormControl 
+                  placeholder="YYYY-MM-DD"
+                  aria-label="YYYY-MM-DD"
+                  aria-describedby="basic-addon2"
+                  onChange = {this.handleDate}
+                  required
+                />
+              </InputGroup>
+            </div>
+            <div>
+              <h4>Enter the Start time : </h4>
+              <InputGroup className='mb-3'>
+                <FormControl 
+                  placeholder="hh:mm"
+                  aria-label="hh:mm"
+                  aria-describedby="basic-addon2"
+                  onChange = {this.handleStartTime}
+                  required
+                />
+              </InputGroup>
+            </div>
+            <div>
+              <h4>Enter the End time : </h4>
+              <InputGroup className='mb-3'>
+                <FormControl 
+                  placeholder="hh:mm"
+                  aria-label="hh:mm"
+                  aria-describedby="basic-addon2"
+                  onChange = {this.handleEndTime}
+                  required
+                />
+              </InputGroup>
+            </div>
+            <div className="createAccount">
+              <br />
+              <Button type="Submit" onClick = {this.onSubmit} style={{backgroundColor: "orange"}}>Submit</Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FitBit
